cd9eb7989ea523889f66f25aa9de1285972890fe
Onurcan Kurt <onurcankurt96@gmail.com>
2024-05-12 15:52:30 +0300

    viewcontroller files added

---
+++ ContactsAppCoreData/PersonCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonCell.swift
+//  ContactsAppCoreData
+//
+//  Created by onur on 12.05.2024.
+//
+
+import UIKit
+
+class PersonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
